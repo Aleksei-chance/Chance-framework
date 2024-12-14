08f2d244ec7d66205d5c4ac9cdfd2349d146c25f
Aleksei-chance <aleksei.chance@gmail.com>
2024-12-15 00:36:02 +0300

    Request

--- core/Application.php
+++ core/Application.php
@@ -6,10 +6,12 @@ class Application
 {
     protected string $uri;
     public static Application $app;
+    public Request $request;
 
     public function __construct()
     {
         self::$app = $this;
         $this->uri = $_SERVER['REQUEST_URI'];
+        $this->request = new Request($this->uri);
     }
 }
